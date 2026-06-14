f0178c2427b8bcc88b1cb5250486f54f1dd7b6ae
abdullahbasir <abdullah.sbasir@gmail.com>
2026-06-14 12:17:02 +0400

    Added a multipart upload handler that allows for multiple parts of data to be uploaded in one request, commonly for large files that need data to be split

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +29,54 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
+
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer file.Close()
+
+	mediaType := header.Header.Get("Content-Type")
+
+	d, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusServiceUnavailable, "Service unavailable", err)
+		return
+	}
+
+	videoData, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Unauthorized ID", err)
+		return
+	}
+	if videoData.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Unauthorized ID", err)
+		return
+	}
+
+	tnail := thumbnail{
+		data:      d,
+		mediaType: mediaType,
+	}
+
+	videoThumbnails[videoData.ID] = tnail
+
+	thumbnailURL := fmt.Sprintf("http://localhost:%s/api/thumbnails/%v", cfg.port, videoData.ID)
+
+	videoData.ThumbnailURL = &thumbnailURL
+
+	err = cfg.db.UpdateVideo(videoData)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't update video", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, videoData)
 }
